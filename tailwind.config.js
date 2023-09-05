64a8743c95774c17cc5c582ddeca9abaf37ab523
Svitlana Mishchuk <svitlana@igtechn.com>
2023-09-05 20:08:42 +0200

    build, added tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  mode: 'jit',
+  content: ["./src/**/*.{html,js,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        'arsenic': '#433E49',
+        'dark-vanilla': '#DBC1AD',
+        'isabelline': '#F3E8EB'
+      },
+    },
+  },
+  plugins: [],
+}
